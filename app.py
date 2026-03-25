eee08e117d5584b9ab4c86ae0abce40e40825f01
PrakarshaVeldandi <prakarshaveldandi@gmail.com>
2026-03-25 22:39:01 +0530

    Update app.py --with flask

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(_name_)
+
 def add(a, b):
-  return a + b
-print("Addition:", add(5,3))
+    return a + b
+
+@app.route("/")
+def home():
+    return "Addition: " + str(add(5, 3))
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
